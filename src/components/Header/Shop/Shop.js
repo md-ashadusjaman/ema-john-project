cf9c43b09a65eaa94d1eab00a23ae5f3ab71b01c
Md.Ashadus Jaman <mdarishjaman@gmail.com>
2021-02-28 21:34:45 +0600

    done first react project

--- src/components/Header/Shop/Shop.js
+++ src/components/Header/Shop/Shop.js
@@ -2,24 +2,41 @@ import React from 'react';
 import fakeData from '../../../fakeData'
 import { useState } from 'react';
 import './Shop.css';
+import Product from '../Product/Product';
+import Cart from '../Cart/Cart';
 
 const Shop = () => {
     // console.log(fakeData);
     const first10 = fakeData.slice(0, 10);
     const [products, setProducts] = useState(first10);
+    const [cart, setCart] = useState([]);
+
+    const handleAddProduct = (product) => {
+        console.log('Product Added', product);
+        const newCart = [...cart, product];
+        setCart(newCart);
+
+    }
+
+
     return (
         <div className='shop-container'>
 
             <div className="product-container">
-                <ul>
-                    {
-                        products.map(product => <li>{product.name}</li>)
-                    }
-                </ul>
+
+                {
+                    products.map(pd => <Product
+                        handleAddProduct={handleAddProduct}
+                        product={pd}></Product>)
+                }
+
             </div>
 
             <div className="cart-container">
-                <h3>this is cart</h3>
+                <Cart cart={cart}></Cart>
+
+                {/* <h3>this is cart</h3>
+                <h5>Order Summery: {cart.length}</h5> */}
             </div>
         </div>
     );
