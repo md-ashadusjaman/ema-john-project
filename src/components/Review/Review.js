6a9886ffae0dc9c83072229f26acd703435eadb8
md-ashadusjaman <mdajasad@gmail.com>
2021-03-09 00:32:42 +0600

    remove items from cart

--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,10 +1,19 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import fakeData from "../../fakeData";
-import { getDatabaseCart } from "../../utilities/databaseManager";
+import { getDatabaseCart, removeFromDatabaseCart } from "../../utilities/databaseManager";
 import ReviewItem from "../ReviewItem/ReviewItem";
 
 const Review = () => {
-  const [cart, setCart ] = useState([]);
+  const [cart, setCart] = useState([]);
+
+  const removeProduct = (productKey) => {
+    // console.log('remove product', productKey);
+
+    const newCart = cart.filter((pd) => pd.key !== productKey);
+    setCart(newCart);
+    removeFromDatabaseCart(productKey);
+  }
+
   useEffect(() => {
     const savedCart = getDatabaseCart();
     // console.log(savedCart);
@@ -13,24 +22,24 @@ const Review = () => {
     // console.log(productKeys);
     // const counts = productKeys.map( key => savedCart[key]);
 
-    const cartProducts = productKeys.map( key => {
-        const product = fakeData.find( pd => pd.key === key);
-        product.quantity = savedCart[key];
-        return product;
-    }, [])
-setCart(cartProducts);
+    const cartProducts = productKeys.map((key) => {
+      const product = fakeData.find((pd) => pd.key === key);
+      product.quantity = savedCart[key];
+      return product;
+    }, []);
+    setCart(cartProducts);
     // console.log(cartProducts);
-
-    
-  });
+  },[]);
   return (
     <div>
       <h1>Cart Items: {cart.length}</h1>
-      {
-          cart.map(pd => <ReviewItem 
-          key= {pd.key}
-          product={pd}></ReviewItem>)
-      }
+      {cart.map((pd) => (
+        <ReviewItem
+          key={pd.key}
+          removeProduct={removeProduct}
+          product={pd}
+        ></ReviewItem>
+      ))}
     </div>
   );
 };
